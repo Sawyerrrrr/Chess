847e1400c64820813b4998871197075b1e9f47e3
Sawyerrrrr <396511618@qq.com>
2023-04-16 23:00:25 +0800

    this is chess game

---
+++ mainwindow.h
@@ -0,0 +1,37 @@
+#ifndef MAINWINDOW_H
+#define MAINWINDOW_H
+
+#include <QMainWindow>
+#include <QAction>
+#include "qchessmap.h"
+#include "severwd.h"
+#include "clientwd.h"
+
+QT_BEGIN_NAMESPACE
+namespace Ui { class MainWindow; }
+QT_END_NAMESPACE
+
+class MainWindow : public QMainWindow
+{
+    Q_OBJECT
+
+public:
+    MainWindow(QWidget *parent = nullptr);
+    ~MainWindow();
+    void initMap();
+
+private:
+    Ui::MainWindow *ui;
+    QChessMap *m_map;
+    bool my=false;
+    bool other=false;
+public slots:
+    void createSever();
+    void createClient();
+    void addressData();
+    void startGame();
+
+private slots:
+    void on_pushButton_clicked();
+};
+#endif // MAINWINDOW_H
